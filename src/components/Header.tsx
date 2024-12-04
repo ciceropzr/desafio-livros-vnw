f57a1382c07da0081a4e3da9ecf428817e239e5d
Cicero <ciceropzr@gmail.com>
2024-12-04 17:23:58 -0300

    Criando projeto e header

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import LogoImg from "../assets/images/book-logo.png";
+import s from "./styles.module.scss";
+import searchImg from "../assets/images/search.png";
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <section className={s.container}>
+        <div>
+          <img src={LogoImg} alt="book" />
+          <p className={s.container__paragraph}>Livros Vai na Web</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Início</Link>
+            </li>
+            <li>
+              <Link to="/livros-doados">Livros Doados</Link>
+            </li>
+            <li>
+              <Link to="/quero-doar">Quero Doar</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={s.container__search}>
+          <input type="search" placeholder="O que você procura?" />
+          <img src={searchImg} alt="search" />
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
