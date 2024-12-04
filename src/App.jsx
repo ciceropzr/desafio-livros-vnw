ac6542e402d9a49f6d54879ab1bebcc5ef601fae
Cicero <ciceropzr@gmail.com>
2024-12-04 19:45:27 -0300

    Ajustes finais

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Header from './components/Header/header'
 import Footer from './components/Footer/footer'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Inicio from './Pages/Inicio/Inicio'
+import Doados from './Pages/Doados/Doados'
+import QueroDoar from './Pages/QueroDoar/QueroDoar'
+
 import './global.scss'
 
-export default function App(){
-  return(
-    <>
-    <Header/>
-    <Footer/>
-    </>
+export default function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/doados" element={<Doados />} />
+            <Route path="/queroDoar" element={<QueroDoar />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -6,22 +6,22 @@ import twitt from '../../assets/twitt.png'
 import youtube from '../../assets/youtube.png'
 import S from './footer.module.scss'
 
-export default function footer(){
-    return(
-        <footer>
-            <section className={S.boxSocial}>
-                <h3>4002-8922</h3>
-                <nav>
-                    <a href="">  <img src={face} alt="" />  </a>
-                    <a href="">  <img src={twitt} alt="" />  </a>
-                    <a href="">  <img src={youtube} alt="" /> </a>
-                    <a href="">  <img src={linkedin} alt="" />  </a>
-                    <a href="">  <img src={insta} alt="" />  </a>
-                </nav>
-            </section>
-            <section className={S.boxTexto}>
-              <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
-            </section>
-        </footer>
-    )
+export default function footer() {
+  return (
+    <footer>
+      <section className={S.boxSocial}>
+        <h3>4002-8922</h3>
+        <nav>
+          <a href="">  <img src={face} alt="" />  </a>
+          <a href="">  <img src={twitt} alt="" />  </a>
+          <a href="">  <img src={youtube} alt="" /> </a>
+          <a href="">  <img src={linkedin} alt="" />  </a>
+          <a href="">  <img src={insta} alt="" />  </a>
+        </nav>
+      </section>
+      <section className={S.boxTexto}>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
+      </section>
+    </footer>
+  )
 }
